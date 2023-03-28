efab53b28b9311ae50c35bd5e37d43b1ac60c773
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2023-03-28 11:19:09 +0300

    add hide header on scroll

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,42 @@
-import { Suspense } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Container from 'components/Container/Container';
 import AppBar from 'components/AppBar/AppBar';
 
 import { Header } from './SharedLayout.styled';
+
 const SharedLayout = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isAppBarVisible, setIsAppBarVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (prevScrollPos > currentScrollPos) {
+        setIsAppBarVisible(true);
+      } else {
+        setIsAppBarVisible(false);
+      }
+
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos, isAppBarVisible]);
+
   return (
     <>
-      <Header>
+      <Header
+        style={{
+          transform: isAppBarVisible ? 'translateY(0)' : 'translateY(-100%)',
+        }}
+      >
         <Container>
           <AppBar />
         </Container>
--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -4,8 +4,10 @@ export const Header = styled.header`
   /* display: flex;
   align-items: center;
   justify-content: center; */
-  /* position: fixed; */
+  position: fixed;
   top: 0px;
   width: 100%;
   background-color: white;
+  transition: transform 0.2s ease-in-out;
+  /* box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.05); */
 `;
