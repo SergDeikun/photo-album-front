6f23eeff588ead576edc134b0fa4cb4bc6719d58
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2024-01-04 01:27:08 +0200

    Update SharedAlbumPage.jsx

--- src/pages/SharedAlbumPage/SharedAlbumPage.jsx
+++ src/pages/SharedAlbumPage/SharedAlbumPage.jsx
@@ -1,20 +1,20 @@
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
-// import useGetAlbumById from 'react-query/useGetAlbumById';
+import useGetAlbumById from 'react-query/useGetAlbumById';
 
-// import PhotoList from 'components/PhotoList/PhotoList';
+import PhotoList from 'components/PhotoList/PhotoList';
 
 import { BoxContainer } from './SharedAlbumPage.styled';
 
 const SharedAlbumPage = () => {
-  // const { albumId } = useParams();
-  // const { data: currentAlbumData, isLoading } = useGetAlbumById(albumId);
+  const { albumId } = useParams();
+  const { data: currentAlbumData, isLoading } = useGetAlbumById(albumId);
 
   return (
     <BoxContainer>
-      {/* {currentAlbumData && (
+      {currentAlbumData && (
         <PhotoList currentAlbumData={currentAlbumData} isLoading={isLoading} />
-      )} */}
+      )}
     </BoxContainer>
   );
 };
