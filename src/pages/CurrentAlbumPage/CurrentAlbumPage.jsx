1db389744e714bb0353270af82b2aaf6b0713338
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2024-01-04 00:10:57 +0200

    Update CurrentAlbumPage.jsx

--- src/pages/CurrentAlbumPage/CurrentAlbumPage.jsx
+++ src/pages/CurrentAlbumPage/CurrentAlbumPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 
-import useGetAlbumById from 'react-query/useGetAlbumById.js';
+import useGetAlbumById from 'react-query/useGetAlbumById';
 
 import PhotoList from 'components/PhotoList/PhotoList';
 
