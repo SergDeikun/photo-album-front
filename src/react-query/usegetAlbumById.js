2506c711ca50119e281ba079c4eb5ceed5302f65
SergDeikun <92675885+SergDeikun@users.noreply.github.com>
2024-01-03 23:44:38 +0200

    update import

--- src/react-query/usegetAlbumById.js
+++ src/react-query/usegetAlbumById.js
@@ -2,10 +2,10 @@ import { useQuery } from 'react-query';
 
 import { getAlbumById } from 'api/api-fetch';
 
-const useGetAlbumById = albumId => {
+export const useGetAlbumById = albumId => {
   const album = useQuery(['album', albumId], () => getAlbumById(albumId));
 
   return album;
 };
 
-export default useGetAlbumById;
+// export default useGetAlbumById;
